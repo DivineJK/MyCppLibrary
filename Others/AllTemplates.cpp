133f23522883a617ac3fdf79609a464cd806bbf2
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-08-14 23:56:45 +0900

    Update AllTemplates.cpp

--- Others/AllTemplates.cpp
+++ Others/AllTemplates.cpp
@@ -17,3 +17,130 @@
 #include <stdexcept>
 #include <cassert>
 using namespace std;
+
+// modint
+template <int m>
+class modint{
+    using mint = modint<m>;
+    private:
+    static constexpr int mod() {return m;}
+    static constexpr unsigned int umod() {return m;}
+    unsigned int x = 0;
+    public:
+    modint() {}
+    template <class T>
+    modint(T a){
+        long long z = (long long)(a % mod());
+        z = (z >= 0 ? z : z + umod());
+        x = (unsigned int)z;
+    }
+    modint(bool a){
+        x = (uint32_t)a;
+    }
+    unsigned int val(){
+        return x;
+    }
+    mint inv() const{
+        mint res = 1;
+        int n = x;
+        int mm = mod();
+        while (n > 1){
+            res *= (mm - mm / n);
+            n = mm % n;
+        }
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& rhs){
+        os << rhs.x;
+        return os;
+    }
+    friend istream& operator>>(istream& ist, mint& rhs){
+        unsigned int s;
+        ist >> s;
+        rhs = s;
+        return (ist);
+    }
+    bool operator==(const mint& rhs){
+        return x == rhs.x;
+    }
+    template <typename T>
+    bool operator==(const T rhs){
+        mint res = mint(rhs);
+        return x == res.x;
+    }
+    bool operator!=(const mint& rhs){
+        return x != rhs.x;
+    }
+    template <typename T>
+    bool operator!=(const T rhs){
+        mint res = mint(rhs);
+        return x != res.x;
+    }
+    mint operator+(){
+        mint res = mint(*this);
+        return res;
+    }
+    mint operator-(){
+        mint res = mint(*this);
+        if (res.x){
+            res.x = umod() - res.x;
+        }
+        return res;
+    }
+    mint& operator++(){
+        x++;
+        if (x == umod()) x = 0;
+        return *this;
+    }
+    mint& operator--(){
+        if (x == 0) x = umod();
+        x--;
+        return *this;
+    }
+    mint operator++(int){
+        mint res = *this;
+        ++*this;
+        return res;
+    }
+    mint operator--(int){
+        mint res = *this;
+        --*this;
+        return res;
+    }
+    mint& operator+=(const mint& rhs){
+        x = (x + rhs.x < umod() ? x + rhs.x : x + rhs.x - umod());
+        return *this;
+    }
+    mint& operator-=(const mint& rhs){
+        x = (x < rhs.x ? x + umod() - rhs.x : x - rhs.x);
+        return *this;
+    }
+    mint& operator*=(const mint& rhs){
+        x = (unsigned int)((long long)x * rhs.x % umod());
+        return *this;
+    }
+    mint& operator/=(const mint& rhs){
+        *this *= rhs.inv();
+        return *this;
+    }
+    mint operator+(const mint& rhs){
+        mint res = mint(*this);
+        res += rhs;
+        return res;
+    }
+    mint operator-(const mint& rhs){
+        mint res = mint(*this);
+        res -= rhs;
+        return res;
+    }
+    mint operator*(const mint& rhs){
+        mint res = mint(*this);
+        res *= rhs;
+        return res;
+    }
+    mint operator/(const mint& rhs){
+        mint res = mint(*this);
+        res /= rhs;
+        return res;
+    }
+};
