294d7aa0f52b067cab06d0215da60f12426e7f9f
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-08-15 00:02:01 +0900

    Update AllTemplates.cpp

--- Others/AllTemplates.cpp
+++ Others/AllTemplates.cpp
@@ -315,3 +315,49 @@ class Dijkstra{
         isDirty = true;
     }
 };
+
+// Union-Find (Disjoint Set Union)
+class UnionFind{
+    public:
+    int num;
+    vector<int> parent;
+    vector<int> size;
+    UnionFind(int n){
+        num = n;
+        parent = vector<int>(num, 0);
+        for (int i=0;i<num;i++){
+            parent[i] = i;
+        }
+        size = vector<int>(num, 1);
+    }
+    int getRoot(int x){
+        int t = x;
+        while (t != parent[t]){
+            t = parent[t];
+        }
+        int s;
+        while (x != parent[x]){
+            s = x;
+            x = parent[x];
+            parent[s] = t;
+        }
+        return t;
+    }
+    bool isSame(int x, int y){
+        return getRoot(x) == getRoot(y);
+    }
+    void unite(int x, int y){
+        x = getRoot(x);
+        y = getRoot(y);
+        if (x == y){
+            return;
+        }
+        if (size[x] < size[y]){
+            int t = x;
+            x = y;
+            y = t;
+        }
+        size[x] += size[y];
+        parent[y] = x;
+    }
+};
